68428abc8681676bd25dcf208311cc80cb3f7a09
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-11-01 21:17:39 +0330

    remove unnecesary escape character

--- src/routes/index/validators.js
+++ src/routes/index/validators.js
@@ -18,7 +18,7 @@ const { default: z } = require("zod")
  */
 const filenameSchema = z
   .string()
-  .regex(/^[a-zA-Z0-9_\-]+\.[a-zA-Z0-9]+$/, { message: "Invalid filename format" })
+  .regex(/^[a-zA-Z0-9_-]+\.[a-zA-Z0-9]+$/, { message: "Invalid filename format" })
   .refine((name) => !name.includes("/") && !name.includes("\\"), { message: "Filename must not contain paths" })
 
 module.exports = { filenameSchema }
